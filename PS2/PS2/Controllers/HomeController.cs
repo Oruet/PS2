2888797813fa0647ce8fe185ef16ded19473238f
Delia Macedolean <deliamacedolean@outlook.com>
2018-05-20 20:24:13 +0300

    Action apasare butoane

--- PS2/PS2/Controllers/HomeController.cs
+++ PS2/PS2/Controllers/HomeController.cs
@@ -4,11 +4,29 @@ namespace PS2.Controllers
 {
     public class HomeController : Controller
     {
+        public bool Umplut { get; set; }
+
         public IActionResult Index()
         {
+           // Umplut = true;
+            ViewData["Umplut"] = Umplut;
             return View();
         }
 
-       
+        public IActionResult Umplere()
+        {
+            Umplut = true;
+            ViewData["Umplut"] = Umplut;
+            return View("Index");
+        }
+
+        public IActionResult Golire()
+        {
+            Umplut = false;
+            ViewData["Umplut"] = Umplut;
+            return View("Index");
+        }
+
+
     }
 }
